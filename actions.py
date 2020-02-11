a3dbd16ba6d52ae26cd4b55f2f1fb332b59d8803
Jitesh Gaikwad <49223934+JiteshGaikwad@users.noreply.github.com>
2020-02-11 18:16:50 +0530

    Add files via upload

---
+++ actions.py
@@ -0,0 +1,40 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+
+def extract_metadata_from_tracker(tracker):
+    events = tracker.current_state()['events']
+    user_events = []
+    for e in events:
+        if e['event'] == 'user':
+            user_events.append(e)
+
+    return user_events[-1]['metadata']
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        metadata=extract_metadata_from_tracker(tracker)
+        
+        print("MeataData: ",metadata)
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
